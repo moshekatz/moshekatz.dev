a7de09df868f6e6e6530c560cd029990a6191632
moshekatz <moshek777@gmail.com>
2021-04-04 20:34:37 +0300

    added descriptions to project and wtiring pages

--- pages/writing.js
+++ pages/writing.js
@@ -6,10 +6,13 @@ import { PostsList } from '@/components/posts';
 export default function Writing({ postsFrontMatter }) {
   return (
     <Container>
-      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black">
+      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 space-y-3">
+        <h1 className="font-bold text-3xl md:text-5xl tracking-tight text-black">
           Writing
         </h1>
+        <p className="font-semibold text-lg text-gray-500">
+          I write. Mostly about code.
+        </p>
         <PostsList postsFrontMatter={postsFrontMatter} />
       </div>
     </Container>

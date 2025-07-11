9b24f11ee1b2336b1d5354b04a844afe69985406
Moshe Katz <moshe.katz@fiverr.com>
2025-07-11 19:45:26 +0300

    ANOTHER ONE

--- og/generateImage.js
+++ og/generateImage.js
@@ -56,8 +56,8 @@ export async function generatePostImage({ title }) {
         flexDirection: "column",
         width: "100%",
         height: "100%",
-        backgroundColor: "rgb(40, 44, 53)",
-        color: "rgba(255, 255, 255, 0.88)",
+        backgroundColor: "rgba(255, 255, 255, 0.88)",
+        color: "rgb(40, 44, 53)",
       }}
     >
       <div
